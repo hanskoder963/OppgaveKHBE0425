15a204fa2dd99a41642f4a357c9a8ce396d4b650
Hans0425 <hhm042025@gmail.com>
2025-04-28 12:15:16 +0000

    gjort 3 c# oppgaver i program.cs

--- Program.cs
+++ Program.cs
@@ -4,36 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        // initalize a new object, from a class
-        Person person = new Person();
-        person.Name = "Marius";
-        person.Age = 39;
+        // // initalize a new object, from a class
+        // Person person = new Person();
+        // person.Name = "Marius";
+        // person.Age = 39;
         
-        Console.WriteLine(person.Name);
-        Console.WriteLine(person.Age);
+        // Console.WriteLine(person.Name);
+        // Console.WriteLine(person.Age);
 
-        string[] shoppingList = {"Milk", "Soda", "Dinner", "Candy", "Fish", "Salmon"}; // fixed size: new string[6]
-        List<string> programmeringLanguages = new List<string>() {
-            "C#", "Java", "Python", "JavaScript", "C", "C++", "Rust", "Go"
-        };
+        // string[] shoppingList = {"Milk", "Soda", "Dinner", "Candy", "Fish", "Salmon"}; // fixed size: new string[6]
+        // List<string> programmeringLanguages = new List<string>() {
+        //     "C#", "Java", "Python", "JavaScript", "C", "C++", "Rust", "Go"
+        // };
 
-        List<int> ints = [1,2,3,4];
+        // List<int> ints = [1,2,3,4];
 
-        for(int i = 0; i < shoppingList.Length; i++) 
-        {
-            Console.WriteLine(shoppingList[i]);
-        }
+        // for(int i = 0; i < shoppingList.Length; i++) 
+        // {
+        //     Console.WriteLine(shoppingList[i]);
+        // }
 
-        foreach(var programming in programmeringLanguages) 
-        {
-            Console.WriteLine(programming);
-        }
+        // foreach(var programming in programmeringLanguages) 
+        // {
+        //     Console.WriteLine(programming);
+        // }
 
+
+        //
+        //**********************************//
+        //
+
+       
         /*
             Oppgave 1:
                 - Lag noen enkle variabler av forksjellige primitive datatyper
                 - Forsøk å skrive ut verdiene som er tildelt variablene ved å bruke Console.WriteLine(); metoden.
         */
+        string currency = "Euro";
+        int money = 20;
+        
+
+        Console.WriteLine(currency);
+        Console.WriteLine(money);
 
 
         /*
@@ -42,12 +54,26 @@ class Program
                 - Forsøk å skrive ut verdiene som befinner seg i arrayet, ved å bruke en for-loop, eller en foreach-loop.
         */
 
+
+        string[] games = ["Marvel Rivals", "Ashes of Creation", "Schedule1", "Overwatch", "DnD"];
+        foreach (string i in games)
+        {
+            Console.WriteLine(i);
+        }
+
         /*
             Oppgave 3:
                 - Forsøk å tildele variabler til en generiske Liste: eksempel: List<int> integers = new List<int>();
                 - Forsøk å skrive ut verdiene i listen, ved å bruke en for-loop eller en foreach-loop.
         */
+        
+        List<int> antalMatVarer = new List<int> { 2, 5, 1, 8, 12 };
 
+        for (int i = 0; i < antalMatVarer.Count; i++)
+        {
+        Console.WriteLine(antalMatVarer[i]);
+        }       
+        
     }
 
     	public static double Add(double a, double b) 
